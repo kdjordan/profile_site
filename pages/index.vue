891812a29563bb73d62cb84b28a1723d65a400bc
kdjordan <k.dean.jordan@gmail.com>
2022-10-16 21:40:53 -0700

    global color now actually working

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <div>
         <section>
             <div class="container mx-auto px-6 text-center md:pt-20 pb-52">
-                <h1 class="max-w-2xl mx-auto mb-10 text-3xl font-bold leading-normal mt-14 md:text-4xl">
+                <h1 class="max-w-4xl mx-auto mb-10 text-4xl font-bold leading-normal mt-14 md:text-4xl">
                     Hello, Thanks for stopping by.
                 </h1>
             </div>
