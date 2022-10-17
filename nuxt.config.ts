541f65486a93bd79a89c36bfe4b7c619874ddc72
kdjordan <k.dean.jordan@gmail.com>
2022-10-16 22:35:38 -0700

    gsap added, scrolling text kinda working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+    modules: ['@nuxt/content'],
+    // buildModules: ['nuxt-gsap-module'],
     css: ['~/assets/css/main.css'],
     build: {
         postcss: {
             postcssOptions: require('./postcss.config.js'),
         },
+        transpile: ['gsap']
     },
+    // buildModules: ['nuxt-gsap-module'],
+    // gsap: {
+    //     /* module options */
+    //   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,39 @@
 <script setup>
+import { gsap } from 'gsap';
+
+// const gsap = ref()
+onMounted(() => {
+    const text = document.querySelector('.the-text')
+    gsap.set(text, {
+		xPercent: '100'
+	});
+    var tl = gsap.timeline();
+	tl.to(text, {
+		ease: "none",
+		duration: 1,
+		xPercent: 0,
+		repeat: -1
+	});
+}) 
+    
+
 </script>
 <template>
     <div>
         <section>
-            <div class="container mx-auto text-center md:pt-20 pb-52">
-                <h1 class="max-w-4xl mx-auto mb-10 text-[15rem] font-bold leading-normal mt-14 md:text-[15rem] flex-row align-center justify-between">
-                    <div class="div">KEVIN</div>
-                    <div class="div">JORDAN</div>
-                </h1>
+            <div class="container mx-auto pb-52 overflow-hidden">
+                <div class="w-full mb-10 font-bold leading-normal flex flex-row align-center justify-between the-text">
+                    <div class="hero-text">KEVIN&nbsp;</div>
+                    <div class="hero-text">JORDAN</div>
+                </div>
             </div>
         </section>
         <!-- <BlogPostList /> -->
     </div>
 </template>
+
+<style>
+.hero-text {
+    font-size: clamp(30rem, 4vw, 4rem);
+}
+</style>
